73870a64223686d73fa3e0a82897bfe3b45c8122
Emory Merryman <emory.merryman@gmail.com>
2018-02-10 11:05:47 +0000

    make sure cleanup happens

---
+++ exec-0.1.0.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+xhost +local: &&
+    cleanup(){
+        xhost -
+    } &&
+    trap cleanup EXIT &&
+    sudo \
+        /usr/bin/docker \
+        run \
+        --interactive \
+        --rm \
+        --label expiry=$(date --date "now + 1 month" +%s) \
+        --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+        --env DISPLAY \
+        rebelplutonium/outer:0.1.0 \
+            "${@}"
