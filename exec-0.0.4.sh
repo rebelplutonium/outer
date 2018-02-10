8b89f50ec0094992e28e209d8a0ddec4b19efea8
Emory Merryman <emory.merryman@gmail.com>
2018-02-10 21:34:34 +0000

    make it self-cleaning

--- exec-0.0.4.sh
+++ exec-0.0.4.sh
@@ -5,4 +5,4 @@ xhost +local: &&
         xhost -
     } &&
     trap cleanup EXIT &&
-    sudo         /usr/bin/docker         --preserve-env         run         --interactive         --rm         --label expiry=$(($(date +%s)+60*60*24*7))         --volume /var/run/docker.sock:/var/run/docker.sock:ro         --env DISPLAY         rebelplutonium/outer:0.0.4             "${@}"    
+    sudo        --preserve-env /usr/bin/docker          run         --interactive         --rm         --label expiry=$(($(date +%s)+60*60*24*7))         --volume /var/run/docker.sock:/var/run/docker.sock:ro         --env DISPLAY         rebelplutonium/outer:0.0.4             "${@}"    
