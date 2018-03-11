75562d553fafe63605f6b06371db05ffc3e755c5
Emory Merryman <emory.merryman@gmail.com>
2018-03-11 23:24:00 +0000

    radical restructuring

---
+++ entrypoint.user.sh
@@ -0,0 +1,142 @@
+#!/bin/sh
+
+cleanup(){
+    sudo docker stop $(cat docker) $(cat middle) &&
+        sudo docker rm -fv $(cat docker) $(cat middle) &&
+        if [ ! -z "${MONIKER}" ]
+        then
+            sudo docker volume ls --quiet --filter label=moniker=${MONIKER} --filter label=expiry | while read VOLUME
+            do
+                if [ $(sudo docker volume inspect --format "{{.Labels.expiry}}" ${VOLUME}) -lt $(date %s) ]
+                then
+                    sudo docker volume rm ${VOLUME}
+                fi
+            done
+        fi
+} &&
+    trap cleanup EXIT &&
+    source /srv/working/public.env &&
+    export GPG_SECRET_KEY="$(cat /srv/working/private/gpg.secret.key)" &&
+    export GPG2_SECRET_KEY="$(cat /srv/working/private/gpg2.secret.key)" &&
+    export GPG_OWNER_TRUST="$(cat /srv/working/public/gpg.owner.trust)" &&
+    export GPG2_OWNER_TRUST="$(cat /srv/working/public/gpg2.owner.trust)" &&
+    export TARGET_UID="$(stat -c %u /srv/working)" &&
+    if [ -z "${CLOUD9_PORT}" ]
+    then
+        echo Unspecified CLOUD9_PORT &&
+            exit 65
+    fi &&
+    if [ -z "${PROJECT_NAME}" ]
+    then
+        echo Unspecified PROJECT_NAME &&
+            exit 66
+    fi &&
+    if [ -z "${USER_NAME}" ]
+    then
+        echo Unspecified USER_NAME &&
+            exit 67
+    fi &&
+    if [ -z "${USER_EMAIL}" ]
+    then
+        echo Unspecified USER_EMAIL &&
+            exit 68
+    fi &&
+    if [ -z "${GPG_SECRET_KEY}" ]
+    then
+        echo Unspecified GPG_SECRET_KEY &&
+            exit 69
+    fi &&
+    if [ -z "${GPG2_SECRET_KEY}" ]
+    then
+        echo Unspecified GPG2_SECRET_KEY &&
+            exit 70
+    fi &&
+    if [ -z "${GPG_OWNER_TRUST}" ]
+    then
+        echo Unspecified GPG_OWNER_TRUST &&
+            exit 71
+    fi &&
+    if [ -z "${GPG2_OWNER_TRUST}" ]
+    then
+        echo Unspecified GPG2_OWNER_TRUST &&
+            exit 72
+    fi &&
+    if [ -z "${SECRETS_ORGANIZATION}" ]
+    then
+        echo Unspecified SECRETS_ORGANIZATION &&
+            exit 73
+    fi &&
+    if [ -z "${SECRETS_REPOSITORY}" ]
+    then
+        echo Unspecified SECRETS_REPOSITORY &&
+            exit 74
+    fi &&
+    if [ -z "${DOCKER_SEMVER}" ]
+    then
+        echo Unspecified DOCKER_SEMVER &&
+            exit 75
+    fi &&
+    if [ -z "${BROWSER_SEMVER}" ]
+    then
+        echo Unspecified BROWSER_SEMVER &&
+            exit 76
+    fi &&
+    if [ -z "${MIDDLE_SEMVER}" ]
+    then
+        echo Unspecified MIDDLE_SEMVER &&
+            exit 77
+    fi &&
+    if [ -z "${TARGET_UID}" ]
+    then
+        echo Unspecified TARGET_UID &&
+            exit 78
+    fi &&
+    IMAGE_VOLUME=$(sudo --preserve-env docker volume ls --quiet --filter label=moniker=${MONIKER} | head -n 1) &&
+    if [ -z "${IMAGE_VOLUME}" ]
+    then
+        IMAGE_VOLUME=$(sudo docker volume create --label moniker=${MONIKER} --label expiry=$(date --date "now + 1 month" %s))
+    fi &&
+    sudo \
+        docker \
+        create \
+        --cidfile docker \
+        --privileged \
+        --volume /:/srv/host:ro \
+        --volume ${IMAGE_VOLUME}:/var/lib/docker \
+        --label expiry=$(date --date "now + 1 month" +%s) \
+        docker:${DOCKER_SEMVER}-ce-dind \
+            --host tcp://0.0.0.0:2376 &&
+    sudo docker start $(cat docker) &&
+    sudo docker exec --interactive $(cat docker) mkdir /opt &&
+    sudo docker exec --interactive $(cat docker) mkdir /opt/cloud9 &&
+    sudo docker exec --interactive $(cat docker) mkdir /opt/cloud9/workspace &&
+    sudo docker exec --interactive $(cat docker) chown $(stat -c %u /srv/working) /opt/cloud9/workspace &&
+    sudo \
+        --preserve-env \
+        docker \
+        create \
+        --cidfile middle \
+        --interactive \
+        --env DISPLAY \
+        --env DOCKER_HOST \
+        --env CLOUD9_PORT \
+        --env PROJECT_NAME \
+        --env USER_NAME \
+        --env USER_EMAIL \
+        --env GPG_SECRET_KEY \
+        --env GPG2_SECRET_KEY \
+        --env GPG_OWNER_TRUST \
+        --env GPG2_OWNER_TRUST \
+        --env GPG_KEY_ID \
+        --env SECRETS_ORGANIZATION \
+        --env SECRETS_REPOSITORY \
+        --env DOCKER_SEMVER \
+        --env BROWSER_SEMVER \
+        --env MIDDLE_SEMVER \
+        --env INNER_SEMVER \
+        --env TARGET_UID \
+        --env DOCKER_HOST=$(sudo --preserve-env docker inspect --format "tcp://{{ .NetworkSettings.Networks.bridge.IPAddress }}:2376" $(cat docker)) \
+        --label expiry=$(date --date "now + 1 month" +%s) \
+        rebelplutonium/middle:${MIDDLE_SEMVER} \
+            "${@}" &&
+    sudo --preserve-env docker start --interactive $(cat middle)
--- exec-0.0.13.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xhost +local: &&
-    cleanup(){
-        xhost -
-    } &&
-    trap cleanup EXIT &&
-    sudo         --preserve-env         /usr/bin/docker         run         --interactive         --rm         --label expiry=$(($(date +%s)+60*60*24*7))         --volume /var/run/docker.sock:/var/run/docker.sock:ro         --env DISPLAY         rebelplutonium/outer:0.0.13             "${@}"    
